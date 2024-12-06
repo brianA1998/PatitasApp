f427eaa7cc37c1edce669c0533183302fb87e88f
brianA1998 <brian.alaniz@hotmail.com>
2024-12-05 22:39:15 -0300

    app funcional, se modifico el manifest, se agregaron el viewmodel de main

--- app/src/main/java/com/example/patitasapp/MainActivity.kt
+++ app/src/main/java/com/example/patitasapp/MainActivity.kt
@@ -3,14 +3,21 @@ package com.example.patitasapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
+import com.example.patitasapp.onboarding.presentation.OnboardingScreen
 import com.example.patitasapp.ui.theme.PatitasAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -19,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Text(text = "Hola!")
+                    OnboardingScreen(onFinish = { viewModel.hasSeenOnboarding })
                 }
             }
         }
--- app/src/main/java/com/example/patitasapp/onboarding/presentation/OnboardingScreen.kt
+++ app/src/main/java/com/example/patitasapp/onboarding/presentation/OnboardingScreen.kt
@@ -1,2 +1,38 @@
 package com.example.patitasapp.onboarding.presentation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.patitasapp.R
+import com.example.patitasapp.onboarding.presentation.components.OnboardingPager
+
+@Composable
+fun OnboardingScreen(
+    onFinish: () -> Unit,
+    viewModel: OnboardingViewModel = hiltViewModel()
+) {
+    LaunchedEffect(key1 = viewModel.hasSeenOnboarding){
+        if(viewModel.hasSeenOnboarding){
+            onFinish()
+        }
+    }
+    val pages = listOf(
+        OnboardingPagerInformation("Welcome to Monumental Habits",
+            "We can help you to be a better version of yourself",
+            R.drawable.ic_launcher_foreground),
+        OnboardingPagerInformation("Create new habits easily",
+            "We can help you to be a better version of yourself",
+            R.drawable.ic_launcher_foreground),
+        OnboardingPagerInformation("Keep track of your progress",
+            "We can help you to be a better version of yourself",
+            R.drawable.ic_launcher_foreground),
+        OnboardingPagerInformation("Join a supportive community",
+            "We can help you to be a better version of yourself",
+            R.drawable.ic_launcher_foreground)
+    )
+
+    OnboardingPager(pages = pages, onFinish = {
+        viewModel.completeOnboarding()
+    })
+
+}
